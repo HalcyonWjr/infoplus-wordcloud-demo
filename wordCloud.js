312993b8d420515f244a96997e01cfb722e5112a
Jinrui Wang <alicewang9805@gmail.com>
2023-11-14 23:14:04 +0000

    update style

--- wordCloud.js
+++ wordCloud.js
@@ -7,13 +7,14 @@ document.addEventListener("DOMContentLoaded", function() {
         return acc;
     }, {});
 
-    const width = window.innerWidth - 400;
-    const height = window.innerHeight - 400;
+    const width = window.innerWidth - 50;
+    const height = window.innerHeight - 100;
 
-    const customColors = ['#6A6ECF', '#5254A3', '#8DA252', '#B5CF6B',
-                          '#393B79', '#9C9EDE', '#637939', '#CFDB9B', 
-                          '#843C38', '#E6969C', '#D172BF', '#DD9ED6', 
-                          '#BE9E39', '#E7BA51', '#8C6D31', '#E7CB94', ];
+    const customColors = ['#19BCBC', '#B01CF5', '#778AFF', '#F8AFAF',
+                          '#750DA6', '#1C3FF5', '#EF7575', '#FBE7A6', 
+                          '#0C27B5', '#D43A3A', '#F7D04E', '#B3FBED', 
+                          '#A52A2A', '#F7D04E', '#68F7DA', '#F79AFB',
+                          '#C8AA45', '#1CF5F5', '#EE36F7', '#BBC4FF'];
 
     const color = d3.scaleOrdinal(customColors);
 
@@ -22,20 +23,38 @@ document.addEventListener("DOMContentLoaded", function() {
             .sort((a, b) => b[1] - a[1])
             .slice(0, topN);
 
-        const fontSizeScale = d3.scaleLinear()
-            .domain([1, wordFrequency[topWords[0][0]]])
-            .range([20, 200]);
+        // repsonsive for fewer words
+        const padding = topN <= 25 ? 2 : 5;
+        const layoutSize = topN <= 25 ? [width * 0.6, height * 0.6] : [width * 0.8, height * 0.8];
+
+        // const layoutSize = [width * 0.8, height * 0.8];
+
+        // absolute font-size scale
+        // const fontSizeScale = d3.scaleLinear()
+        //     .domain([1, wordFrequency[topWords[0][0]]])
+        //     .range([30, 180]);
+
+        // normalise the font-size scale
+        const maxFreq = topWords[0][1]; 
+        const minFreq = topWords[topWords.length - 1][1];
+        const fontSizeScale = d3.scaleSqrt()
+            .domain([Math.sqrt(minFreq), Math.sqrt(maxFreq)])
+            .range([30, 180]);
 
         const layout = d3.layout.cloud()
-            .size([width, height])
+            .size(layoutSize)
             .words(topWords.map(function(d, i) {
-                return {text: d[0], size: fontSizeScale(d[1]), color: color(i)};
+                // degree by N
+                // return {text: d[0], size: fontSizeScale(d[1]), color: color(i)};
+
+                // degree by squareroot N
+                return {text: d[0], size: fontSizeScale(Math.sqrt(d[1])), color: color(i)};
             }))
-            .padding(5)
+            .padding(padding)
             .rotate(0)
             .font("Impact")
             .fontSize(function(d) { return d.size; })
-            .spiral("archimedean")
+            .spiral("rectangular")
             .on("end", draw);
 
         layout.start();
